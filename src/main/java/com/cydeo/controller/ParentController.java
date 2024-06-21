b3cce651afc1e0fd1ff526543d0de82436a64b55
martinasenov <martin.manuilov.asenov@gmail.com>
2024-06-22 01:03:57 +0200

    ParentController is completed

--- src/main/java/com/cydeo/controller/ParentController.java
+++ src/main/java/com/cydeo/controller/ParentController.java
@@ -1,6 +1,37 @@
 package com.cydeo.controller;
 
+
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.ParentService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/parent")
 public class ParentController {
+
+    private final ParentService parentService;
+
+
+    public ParentController(ParentService parentService) {
+        this.parentService = parentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getParents(){
+
+        ResponseWrapper parents=new ResponseWrapper("Parents are successfully retrieved.",parentService.findAll());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Parents", "Returned");
+
+        return ResponseEntity.ok().headers(headers).body(parents);
+
+    }
 
      /*
            Endpoint: /api/v1/parent
